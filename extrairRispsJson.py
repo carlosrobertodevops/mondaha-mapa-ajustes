54836afd0a562d7fcd14c79eb1c7af3525def6fd
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-04-04 12:54:44 -0300

    Ajustes

--- extrairRispsJson.py
+++ extrairRispsJson.py
@@ -2,18 +2,28 @@ import json
 import os
 
 # Carregar o arquivo GeoJSON
-with open("risps.geojson", "r", encoding="utf-8") as file:
+with open("./dados/risps.geojson", "r", encoding="utf-8") as file:
     geojson_data = json.load(file)
 
 # Criar diretório de saída se não existir
 output_dir = "output_risps_coordinates"
 os.makedirs(output_dir, exist_ok=True)
 
+def sanitize_filename(name):
+        # Normaliza os caracteres acentuados para sua versão base (exemplo: "é" → "e")
+    name = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode('ASCII')
+    """Remove caracteres inválidos para nomes de arquivos."""
+
 # Percorrer todas as features do arquivo
 for feature in geojson_data["features"]:
     properties = feature["properties"]
-    region_name = properties.get("RISP", 0)  # Nome do município como chave
-    file_path = os.path.join(output_dir, f"RISP_{region_name}.json")
+    region_name = properties.get("RISP")  # Nome do município como chave
+
+    if not region_name:
+        continue  # Pula caso não haja nome definido
+
+    region_name = sanitize_filename(region_name)  # Nome sanitizado para arquivo
+    file_path = os.path.join(output_dir, f"{region_name}.json")
 
     jsonb_formatted = []
     geometry = feature["geometry"]
